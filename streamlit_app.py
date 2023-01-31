d80c83db6669e705ff4e08cf481cdb840a0bf6e4
Roman Hanzlik <emiliobig@gmail.com>
2023-02-01 00:41:53 +0100

    added json to pandas and output it

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# cast json to pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output pandas's dataframe
+streamlit.dataframe(fruityvice_normalized)
