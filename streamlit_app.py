f505a08759d612e9625e9d73ed29d04d50d15766
Roman Hanzlik <emiliobig@gmail.com>
2023-02-01 00:42:23 +0100

    removed raw json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # cast json to pandas
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
